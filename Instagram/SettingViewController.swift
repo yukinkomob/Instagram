556a5c5430c1de000deb9d63ae746867c729d068
Yuki Nishimura <yukinkopen@gmail.com>
2021-12-03 12:58:43 +0900

    表示名変更ボタンをタップしたときにFirebaseへ保存する

--- Instagram/SettingViewController.swift
+++ Instagram/SettingViewController.swift
@@ -14,6 +14,28 @@ class SettingViewController: UIViewController {
     @IBOutlet weak var displayNameTextField: UITextField!
     
     @IBAction func handleChangeButton(_ sender: Any) {
+        if let displayName = displayNameTextField.text {
+            if displayName.isEmpty {
+                SVProgressHUD.showError(withStatus: "表示名を入力して下さい")
+                return
+            }
+            
+            let user = Auth.auth().currentUser
+            if let user = user {
+                let changeRequest = user.createProfileChangeRequest()
+                changeRequest.displayName = displayName
+                changeRequest.commitChanges { error in
+                    if let error = error {
+                        SVProgressHUD.showError(withStatus: "表示名の変更に失敗しました。")
+                        print("DEBUG_PRINT: " + error.localizedDescription)
+                        return
+                    }
+                    print("DEBUG_PRINT: [displayName = \(user.displayName!)]の設定に成功しました。")
+                    SVProgressHUD.showSuccess(withStatus: "表示名を変更しました")
+                }
+            }
+        }
+        self.view.endEditing(true)
     }
     
     @IBAction func handleLogoutButton(_ sender: Any) {
