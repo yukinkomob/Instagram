478e175347d5fcc73e8bbb9c0d465d193bd08149
Yuki Nishimura <yukinkopen@gmail.com>
2021-12-05 10:46:51 +0900

    UIImagePickerControllerを使用してライブラリ、カメラの画面を起動する処理を追加

--- Instagram/ImageSelectViewController.swift
+++ Instagram/ImageSelectViewController.swift
@@ -7,7 +7,29 @@
 
 import UIKit
 
-class ImageSelectViewController: UIViewController {
+class ImageSelectViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBAction func handleLibraryButton(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let pickerController = UIImagePickerController()
+            pickerController.delegate = self
+            pickerController.sourceType = .photoLibrary
+            self.present(pickerController, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func handleCameraButton(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let pickerController = UIImagePickerController()
+            pickerController.delegate = self
+            pickerController.sourceType = .camera
+            self.present(pickerController, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func handleCancelButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +37,16 @@ class ImageSelectViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if info[.originalImage] != nil {
+            let image = info[.originalImage] as! UIImage
+            print("DEBUG_PRINT: image = \(image)")
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
