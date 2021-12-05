5642ab873c338d625dc1fb0b059ff029a73a2974
Yuki Nishimura <yukinkopen@gmail.com>
2021-12-05 11:30:48 +0900

    画像加工画面を表示する

--- Instagram/ImageSelectViewController.swift
+++ Instagram/ImageSelectViewController.swift
@@ -5,9 +5,10 @@
 //  Created by Mac on 2021/12/01.
 //
 
+import CLImageEditor
 import UIKit
 
-class ImageSelectViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ImageSelectViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLImageEditorDelegate {
     
     @IBAction func handleLibraryButton(_ sender: Any) {
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
@@ -41,13 +42,27 @@ class ImageSelectViewController: UIViewController, UIImagePickerControllerDelega
         if info[.originalImage] != nil {
             let image = info[.originalImage] as! UIImage
             print("DEBUG_PRINT: image = \(image)")
+            let editor = CLImageEditor(image: image)!
+            editor.delegate = self
+            editor.modalPresentationStyle = .fullScreen
+            picker.present(editor, animated: true, completion: nil)
         }
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         self.presentingViewController?.dismiss(animated: true, completion: nil)
     }
-
+    
+    func imageEditor(_ editor: CLImageEditor!, didFinishEditingWith image: UIImage!) {
+        let postViewController = self.storyboard?.instantiateViewController(withIdentifier: "Post") as! PostViewController
+        postViewController.image = image!
+        editor.present(postViewController, animated: true, completion: nil)
+    }
+    
+    func imageEditorDidCancel(_ editor: CLImageEditor!) {
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
