de433884072d7f6621ec42c79dbc637e00da0e02
Yuki Nishimura <yukinkopen@gmail.com>
2021-12-05 17:44:18 +0900

    投稿機能を実装

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Yuki Nishimura on 2021/12/05.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
--- Instagram/PostViewController.swift
+++ Instagram/PostViewController.swift
@@ -17,9 +17,33 @@ class PostViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     
     @IBAction func handlePostButton(_ sender: Any) {
+        let imageData = image.jpegData(compressionQuality: 0.75)
+        let postRef = Firestore.firestore().collection(Const.PostPath).document()
+        let imageRef = Storage.storage().reference().child(Const.ImagePath).child(postRef.documentID + ".jpg")
+        SVProgressHUD.show()
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        imageRef.putData(imageData!, metadata: metadata) { (metadata, error) in
+            if error != nil {
+                print(error!)
+                SVProgressHUD.showError(withStatus: "画像のアップロードが失敗しました")
+                UIApplication.shared.windows.first{ $0.isKeyWindow }?.rootViewController?.dismiss(animated: true, completion: nil)
+                return
+            }
+            let name = Auth.auth().currentUser?.displayName
+            let postDic = [
+                "name": name!,
+                "caption": self.textField.text!,
+                "date": FieldValue.serverTimestamp(),
+                ] as [String: Any]
+            postRef.setData(postDic)
+            SVProgressHUD.showSuccess(withStatus: "投稿しました")
+            UIApplication.shared.windows.first{ $0.isKeyWindow }?.rootViewController?.dismiss(animated: true, completion: nil)
+        }
     }
     
     @IBAction func handleCancelButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
