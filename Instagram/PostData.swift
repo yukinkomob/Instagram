1af2dc33dcce56c9d0a0eede940b910be225d999
Yuki Nishimura <yukinkopen@gmail.com>
2021-12-13 08:51:54 +0900

    投稿データのクラスを作成 投稿データを表示するカスタムTableViewCellを作成

---
+++ Instagram/PostData.swift
@@ -0,0 +1,35 @@
+//
+//  PostData.swift
+//  Instagram
+//
+//  Created by Yuki Nishimura on 2021/12/05.
+//
+
+import UIKit
+import Firebase
+
+class PostData: NSObject {
+    var id: String
+    var name: String?
+    var caption: String?
+    var date: Date?
+    var likes: [String] = []
+    var isLiked: Bool = false
+    
+    init(document: QueryDocumentSnapshot) {
+        self.id = document.documentID
+        let postDic = document.data()
+        self.name = postDic["name"] as? String
+        self.caption = postDic["caption"] as? String
+        let timestamp = postDic["date"] as? Timestamp
+        self.date = timestamp?.dateValue()
+        if let likes = postDic["likes"] as? [String] {
+            self.likes = likes
+        }
+        if let myid = Auth.auth().currentUser?.uid {
+            if self.likes.firstIndex(of: myid) != nil {
+                self.isLiked = true
+            }
+        }
+    }
+}
---
+++ Instagram/PostTableViewCell.swift
@@ -0,0 +1,57 @@
+//
+//  PostTableViewCell.swift
+//  Instagram
+//
+//  Created by Yuki Nishimura on 2021/12/06.
+//
+
+import UIKit
+import FirebaseStorageUI
+import SDWebImage
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setPostData(_ postData: PostData) {
+        postImageView.sd_imageIndicator = SDWebImageActivityIndicator.gray
+        let imageRef = Storage.storage().reference().child(Const.ImagePath).child(postData.id + ".jpg")
+        postImageView.sd_setImage(with: imageRef)
+        
+        self.captionLabel.text = "\(postData.name!) : \(postData.caption!)"
+        
+        self.dateLabel.text = ""
+        if let date = postData.date {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd HH:mm"
+            let dateString = formatter.string(from: date)
+            self.dateLabel.text = dateString
+        }
+        
+        let likeNumber = postData.likes.count
+        likeLabel.text = "\(likeNumber)"
+        
+        if postData.isLiked {
+            let buttonImage = UIImage(named: "like_exist")
+            self.likeButton.setImage(buttonImage, for: .normal)
+        } else {
+            let buttonImage = UIImage(named: "like_none")
+            self.likeButton.setImage(buttonImage, for: .normal)
+        }
+    }
+}
