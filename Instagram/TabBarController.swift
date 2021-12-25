a3eff45be68a5388bb3fc7acf9507f95557090f0
Yuki Nishimura <yukinkopen@gmail.com>
2021-12-25 09:33:10 +0900

    タブバーの背景色の指定を修正

--- Instagram/TabBarController.swift
+++ Instagram/TabBarController.swift
@@ -8,12 +8,16 @@
 import Firebase
 import UIKit
 
+@available(iOS 15.0, *)
 class TabBarController: UITabBarController, UITabBarControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBar.tintColor = UIColor(red: 1.0, green: 0.44, blue: 0.11, alpha: 1)
-        self.tabBar.barTintColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
+        let appearance = UITabBarAppearance()
+        appearance.backgroundColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
+        self.tabBar.standardAppearance = appearance
+        self.tabBar.scrollEdgeAppearance = appearance
         self.delegate = self
     }
     
